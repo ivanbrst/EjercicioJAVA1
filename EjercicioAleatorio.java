dbc35ce7d38b3ab438a91c04069ccbb1918b56ed
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-12 09:51:20 +0100

    Empezado .java

--- EjercicioAleatorio.java
+++ EjercicioAleatorio.java
@@ -1,1 +1,9 @@
+import java.utilScanner;
+public class EjercicioJAVA1 {
+	public static void main (String args[]){
 
+		Random r= new Random();
+		int valorDado= r.nextInt(6)+1; // Entre 0 y 5, más 1.
+		
+	}
+}
